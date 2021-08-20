62f8a412c5adcfa82b2c4813e1e18b02f8a95dd8
ganeshalapati <alapatiganesh31@gmail.com>
2021-08-21 04:39:56 +0530

    Added for invoice summary

---
+++ src/main/java/Cab_Invoice_Generatorr/InvoiceSummary.java
@@ -0,0 +1,25 @@
+package Cab_Invoice_Generatorr;
+
+public class InvoiceSummary {
+	public double avgFarePerRide;
+    public int totalNoOfRides;
+    public double totalFare;
+
+    public InvoiceSummary(int totalNoOfRides, double totalFare) {
+        this.totalNoOfRides = totalNoOfRides;
+        this.totalFare = totalFare;
+        this.avgFarePerRide = this.totalFare/this.totalNoOfRides;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        InvoiceSummary summary = (InvoiceSummary) o;
+        return Double.compare(summary.avgFarePerRide, avgFarePerRide) == 0 &&
+                totalNoOfRides == summary.totalNoOfRides &&
+                Double.compare(summary.totalFare, totalFare) == 0;
+    }
+
+
+}
