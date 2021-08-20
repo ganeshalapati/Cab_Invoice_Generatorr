64097c2ef6b0ae43235a5532dd5ef98924cad708
ganeshalapati <alapatiganesh31@gmail.com>
2021-08-21 04:36:58 +0530

    Added fares for multiple rides

---
+++ src/main/java/Cab_Invoice_Generatorr/Ride.java
@@ -0,0 +1,12 @@
+package Cab_Invoice_Generatorr;
+
+public class Ride {
+	public int time;
+    public double distance;
+
+    public Ride(double distance, int time) {
+        this.distance = distance;
+        this.time = time;
+
+    }
+}
